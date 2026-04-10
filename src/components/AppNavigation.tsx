8097639d114dc850b2b20a15138312ade7a6f4df
PB <afabianabarrios@gmail.com>
2026-04-10 04:28:43 -0300

    logo

--- src/components/AppNavigation.tsx
+++ src/components/AppNavigation.tsx
@@ -31,7 +31,7 @@ export default function AppNavigation({ email, onLogout }: AppNavigationProps) {
             <img
               src="/isotipocup.png"
               alt="Cupertino"
-              className="h-[40px] w-auto object-contain md:h-[46px]"
+              className="h-[44px] w-auto object-contain md:h-[50px]"
             />
           </Link>
 
